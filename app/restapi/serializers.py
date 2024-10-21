1f4162fd030e7462007c3ff4930674b47f2e287c
Rodrigo Vasconcelos do Carmo <05162565210@aj.local>
2024-10-21 12:53:01 -0400

    Adcionando link para posteriores estudos

--- app/restapi/serializers.py
+++ app/restapi/serializers.py
@@ -4,36 +4,28 @@ from .models import TamPizzas, Cliente, Pedido, Sabores
 
 class TamPizzasSerializer(serializers.ModelSerializer):
     class Meta:
-        #extra_kwargs = {
-        #    'id': {'write_only': True}   ---> FUNÇÃO PARA PROTEGER ALGUNS CAMPOS QUE NÃO VÃO SER APRESENTADOS NO SWAGGER DA API
-        #}
-
         model = TamPizzas
-        fields = (
-            'id',
-            'name_tamanho'    
-        )
+        fields = '__all__'
 
 
 class ClienteSerializer(serializers.ModelSerializer):
-    extra_kwargs = {
-            'cpf_cliente': {'write_only': True},   #---> FUNÇÃO PARA PROTEGER ALGUNS CAMPOS QUE NÃO VÃO SER APRESENTADOS NO SWAGGER DA API
-            'created_at': {'write_only': True}
-        }
-        
+       
     class Meta:
-        model = TamPizzas
-        fields = (
-            'id',
-            'name_cliente',
-            'cpf_cliente',
-            'date_aniver',
-            'created_at'    
-        )
+        extra_kwargs = {
+            'cpf_cliente': {'write_only': True}   #---> FUNÇÃO PARA PROTEGER ALGUNS CAMPOS QUE NÃO VÃO SER APRESENTADOS NO SWAGGER DA API
+        }
 
+        model = Cliente
+        fields = '__all__'
 
 class PedidoSerializer(serializers.ModelSerializer):
     pass
 
 class SaboresSerializer(serializers.ModelSerializer):
-    pass
+
+    class Meta:
+        model = Sabores
+        fields = '__all__'
+
+
+
